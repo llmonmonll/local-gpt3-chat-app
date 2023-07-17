38f27d1a1a79d4ac7d86c7a73a9bfc2817af6a95
llmonmonll <102885097+llmonmonll@users.noreply.github.com>
2023-07-17 20:40:55 +0900

    Add files via upload

--- template/app.js
+++ template/app.js
@@ -1,22 +1,7 @@
 // Define namespace for app code
 var myApp = myApp || {};
 
-myApp.app = (function () {
-	// Initialize app
-	function init() {
-		inputAPIKey.addEventListener('keypress', handleAPIKeyPress);
-		userInput.addEventListener('keypress', handleKeyPress);
-		saveApiKeyButton.addEventListener('click', saveApiKey);
-		apiKeyCheck();
-		removeOldMessages();
-	}
-	// Private variables and functions
-	let inputAPIKey = document.getElementById('apiKey');
-	let userInput = document.getElementById('userInput');
-	let saveApiKeyButton = document.getElementById('saveApiKey');
-	let messagesContainer = document.getElementById('messages');
-	let messagesHistory = [];
-
+myApp.app = (() => {
 	// Store API keys in local storage
 	function saveApiKey() {
 		const apiKeyInput = document.getElementById('apiKey');
@@ -33,12 +18,24 @@ myApp.app = (function () {
 	// Check local storage API keys
 	function apiKeyCheck() {
 		const apiKeyInput = document.getElementById('apiKey');
-		if (localStorage.getItem('apiKey')) {
-			apiKeyInput.placeholder = '✅API Key found';
+		const apiKey = localStorage.getItem('apiKey');
+
+		if (apiKey) {
+			if (validateApiKey(apiKey)) {
+				apiKeyInput.placeholder = '✅API Key found';
+			} else {
+				apiKeyInput.placeholder = '✖Invalid API Key';
+			}
 		} else {
 			apiKeyInput.placeholder = '✖API Key not found';
 		}
 	}
+
+	function validateApiKey(apiKey) {
+		const keyFormat = /^sk-[A-Za-z0-9]+$/;
+		return keyFormat.test(apiKey);
+	}
+
 
 	function handleAPIKeyPress(event) {
 		if (event.key === 'Enter') {
@@ -102,25 +99,25 @@ myApp.app = (function () {
 		messageElement.className = isUserMessage ? 'user-message' : 'ai-message';
 		messageElement.innerHTML = formatMessage(message);
 
-		messagesContainer.appendChild(messageElement);
+		myApp.init.messagesContainer.appendChild(messageElement);
 
 
 		// Adjust scroll position
 		const userInputRect = userInput.getBoundingClientRect();
 		const userInputBottom = userInputRect.top + userInputRect.height;
-		const messagesContainerRect = messagesContainer.getBoundingClientRect();
+		const messagesContainerRect = myApp.init.messagesContainer.getBoundingClientRect();
 		const messagesContainerTop = messagesContainerRect.top;
 
 		if (userInputBottom > messagesContainerTop) {
-			messagesContainer.scrollTop -= userInputBottom - messagesContainerTop;
+			myApp.init.messagesContainer.scrollTop -= userInputBottom - messagesContainerTop;
 		}
 
 		// Message retention period (in milliseconds) 1 week
 
 		// Scroll to the bottom
-		messagesContainer.scrollTop = messagesContainer.scrollHeight;
-		messagesHistory.push({ content: message, isUserMessage, timestamp: Date.now() });
-		localStorage.setItem('messagesHistory', JSON.stringify(messagesHistory));
+		myApp.init.messagesContainer.scrollTop = myApp.init.messagesContainer.scrollHeight;
+		myApp.init.messagesHistory.push({ content: message, isUserMessage, timestamp: Date.now() });
+		localStorage.setItem('messagesHistory', JSON.stringify(myApp.init.messagesHistory));
 
 		removeOldMessages();
 	}
@@ -129,21 +126,21 @@ myApp.app = (function () {
 	function removeOldMessages() {
 		const currentTime = Date.now();
 		const messageRetentionPeriod = 7 * 24 * 60 * 60 * 1000;
-		const updatedMessages = messagesHistory.filter((message) => {
+		const updatedMessages = myApp.init.messagesHistory.filter((message) => {
 			return currentTime - message.timestamp <= messageRetentionPeriod;
 		});
-		messagesHistory = updatedMessages;
-		localStorage.setItem('messagesHistory', JSON.stringify(messagesHistory));
+		myApp.init.messagesHistory = updatedMessages;
+		localStorage.setItem('messagesHistory', JSON.stringify(myApp.init.messagesHistory));
 	}
 	// Show loading message
 	function showLoadingMessage() {
 		const loadingMessage = document.createElement('p');
 		loadingMessage.id = 'loading-message';
 		loadingMessage.textContent = 'Loading...';
-		messagesContainer.appendChild(loadingMessage);
+		myApp.init.messagesContainer.appendChild(loadingMessage);
 
 		// Scroll to the bottom
-		messagesContainer.scrollTop = messagesContainer.scrollHeight;
+		myApp.init.messagesContainer.scrollTop = myApp.init.messagesContainer.scrollHeight;
 	}
 
 	// Remove loading message
@@ -171,8 +168,11 @@ myApp.app = (function () {
 
 		return formattedLines.join('<br>');
 	}
-	// Public API
 	return {
-		init
+		handleAPIKeyPress,
+		handleKeyPress,
+		saveApiKey,
+		apiKeyCheck,
+		removeOldMessages,
 	};
 })();
--- template/init.js
+++ template/init.js
@@ -1,17 +1,30 @@
 var myApp = myApp || {};
 
-myApp.init = (function () {
+myApp.init = (() => {
+	// Private variables and functions
+	let inputAPIKey = document.getElementById('apiKey');
+	let userInput = document.getElementById('userInput');
+	let saveApiKeyButton = document.getElementById('saveApiKey');
+	let messagesContainer = document.getElementById('messages');
+	let messagesHistory = [];
 	// Initialize app
 	function init() {
-		myApp.app.init();
+		inputAPIKey.addEventListener('keypress', myApp.app.handleAPIKeyPress);
+		userInput.addEventListener('keypress', myApp.app.handleKeyPress);
+		saveApiKeyButton.addEventListener('click', myApp.app.saveApiKey);
+		myApp.app.apiKeyCheck();
+		myApp.app.removeOldMessages();
 	}
 	// Public API
 	return {
-		init
+		init,
+		messagesContainer,
+		messagesHistory,
 	};
 })();
 
 // Initialize app when page loads
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', () => {
 	myApp.init.init();
+	myApp.init.messagesHistory = myApp.init.messagesHistory || [];
 });
